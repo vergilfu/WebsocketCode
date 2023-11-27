459e9d3e363ebbbdf9c35cf9fd6a7508fafb55e0
Vergil <157215961@qq.com>
2023-11-27 15:57:47 +0800

    123

---
+++ main_websocket.py
@@ -0,0 +1,15 @@
+#_*_coding:utf-8_*_
+#!/usr/bin/env python
+#Author:Vergil_Fu
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        print(message)
+        await websocket.send(message)
+
+start_server = websockets.serve(echo, "localhost", 8090)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
